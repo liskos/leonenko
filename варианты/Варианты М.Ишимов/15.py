7cfb297c511b4d657d755b6570cfeaafc5991b94
kos <liskosnik@yandex.ru>
2024-03-05 18:53:31 +0300

    05/03/2024

--- варианты/Варианты М.Ишимов/15.py
+++ варианты/Варианты М.Ишимов/15.py
@@ -1,6 +1,8 @@
+
+
 for x in range(1, 10000):
     for A in range(1, 10000):
-        f = (not (120 <= x <= 130) or not (x % 7 == 0)) or (A > 2 * x)
+        f = all([(not (120 <= x <= 130) or not (x % 7 == 0)) or (A > 2 * x) for x in range(1, 10000)])
         if f:
             print(A)
             break
--- варианты/Варианты М.Ишимов/2/14.py
+++ варианты/Варианты М.Ишимов/2/14.py
@@ -1,3 +1,7 @@
-s1 = f'584H84HJ4'
-s2 = f'486B8H1'
-print(int(s1,20) + int(s2, 20))
+for x in '0123456789abcdefghij':
+    a = all([(int(f'5{y}4{x}{y}4HJ4', 20) + int(f'4{y}6B{y}{x}1', 20)) % 15 == 0 for y in '0123456789abcdefghij'])
+    if a:
+        print(x)
+y = 8
+x = 'h'
+print((int(f'5{y}4{x}{y}4HJ4', 20) + int(f'4{y}6B{y}{x}1', 20)))
